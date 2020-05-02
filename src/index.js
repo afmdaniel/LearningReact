e4923f559ff194f95668f8727c6f9113d2ea02c9
afmdanielwin <danielfarias380@gmail.com>
2020-05-02 19:31:07 -0300

    first letter to uppercase as recommended

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import FirstComponent from './components/first'
+import FirstComponent from './components/First'
 
 ReactDOM.render(<FirstComponent />, document.getElementById('root'))
